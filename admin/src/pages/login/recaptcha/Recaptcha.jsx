dc426c8b427e1e986e8da3488a703914870bb413
iDragonius <rustamovruslan1205@gmail.com>
2022-08-18 11:51:03 +0400

    1.22

--- admin/src/pages/login/recaptcha/Recaptcha.jsx
+++ admin/src/pages/login/recaptcha/Recaptcha.jsx
@@ -1,7 +1,7 @@
 import styles from '../Login.module.scss'
 import ReCAPTCHA from 'react-google-recaptcha'
 const Recaptcha = ({ change, refTo }) => {
-    const key = '6LexJUchAAAAAEkbZCxZMQnC02eoNTHNEfHi8VlE'
+    const key = '6LeL3ochAAAAAEfsp8_oQgTZmoV3Te1dXm1JxaqQ'
     return (
         <div className={styles.recaptcha}>
             <ReCAPTCHA sitekey={key} onChange={change} ref={refTo} />
